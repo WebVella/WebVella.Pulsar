c963b9f1ac46f86247e78c2b8cbb47d9e4b9c1d3
Boz Zashev <boz@webvella.com>
2020-08-11 12:18:20 +0300

    Dev

--- WebVella.Pulsar/Models/WvpFileInfo.cs
+++ WebVella.Pulsar/Models/WvpFileInfo.cs
@@ -9,15 +9,6 @@ namespace WebVella.Pulsar.Models
 {
 	public class WvpFileInfo
 	{
-		//Should be removed once we migrate WvpField to the new input
-		private WvpFieldFile Owner { get; set; }
-
-		//Should be removed once we migrate WvpField to the new input
-		public async Task WriteToStreamAsync(Stream stream)
-		{
-			await Owner.WriteToStreamAsync(this, stream, Owner);
-		}
-
 		public long Id { get; set; }
 
 		public DateTime LastModified { get; set; }
@@ -40,11 +31,6 @@ namespace WebVella.Pulsar.Models
 
 		public long ProgressValue = 0;
 
-		//Should be removed once we migrate WvpField to the new input
-		public void Init(WvpFieldFile fileUpload)
-		{
-			Owner = fileUpload;
-		}
 
 	}
 }
