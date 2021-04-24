51b2681a3d6db7df2c93da3721455eb73eb2617e
Rumen Yankov <rumen.yankov@gmail.com>
2021-04-24 19:49:13 +0300

    json converter added

--- WebVella.Pulsar/Utils/Converters.cs
+++ WebVella.Pulsar/Utils/Converters.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
+using System.Runtime.Serialization.Formatters;
 using WebVella.Pulsar.Models;
 
 namespace WebVella.Pulsar.Utils
@@ -709,5 +710,47 @@ namespace WebVella.Pulsar.Utils
 			return sizeString;
 		}
 
+	}
+
+	internal sealed class PrimitiveJsonConverter : JsonConverter
+	{
+		public PrimitiveJsonConverter() { }
+
+		public override bool CanRead { get { return false; } }
+
+		public override bool CanConvert(Type objectType) { return objectType.IsPrimitive || objectType.IsValueType; }
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			switch (serializer.TypeNameHandling)
+			{
+				case TypeNameHandling.All:
+					writer.WriteStartObject();
+					writer.WritePropertyName("$type", false);
+
+					switch (serializer.TypeNameAssemblyFormat)
+					{
+						case FormatterAssemblyStyle.Full:
+							writer.WriteValue(value.GetType().AssemblyQualifiedName);
+							break;
+						default:
+							writer.WriteValue(value.GetType().FullName);
+							break;
+					}
+
+					writer.WritePropertyName("$value", false);
+					writer.WriteValue(value);
+					writer.WriteEndObject();
+					break;
+				default:
+					writer.WriteValue(value);
+					break;
+			}
+		}
 	}
 }
